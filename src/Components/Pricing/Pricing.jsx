ce227b435753f7b98a3575be9830c9b4f439738a
Mithun Mohandas <160577864+mithun-snive@users.noreply.github.com>
2024-04-20 20:37:11 +0530

    pricing card added

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../Header/Header'
 import "./pricing.css"
 
@@ -10,6 +10,7 @@ function Pricing() {
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
     }
+    const [Multiplier, setMultiplier] = useState(1)
 
 
     return (
@@ -39,8 +40,42 @@ function Pricing() {
                     <p className=" text-lg font-light mb-4 ">Meet your goals and accomplish more with Advanced AI features.</p>
                     <h1 className="font-semibold text-4xl mb-4">$20/month*</h1>
 
+                    <br />
+                    
+                    <div className="h-16 mb-3">
+
+                            <div className="">
+                                <h5 className="text-start font-medium  text-violetTxt">{Multiplier}x</h5>
+                            </div>
+                        <div className="flex items-center justify-center h-full">
+                            {/* slider */}
+                            <input
+                                type="range"
+                                id="Multiplier"
+                                name="Multiplier"
+                                list="markers"
+                                min="1"
+                                max="12"
+                                className="w-[70%] accent-white border-gray-300 cursor-ew-resize"
+                                style={{
+                                    height: '50px', // Set height to 30px
+                                    borderRadius: '1.5rem', // Set border radius to half of the height for a circular slider
+                                    background: 'black', // Set inner part color to black
+                                    outline: 'white solid 1px', // Set white outline
+                                    appearance: 'none', // Remove default styles
+                                }}
+                                value={Multiplier}
+                                onChange={(e) => setMultiplier(parseInt(e.target.value))}
+                            />
+                        </div>
+
+
+                    </div>
+
                     <div className="flex items-center justify-center">
-                        <button className="text-black justify-center px-6 py-1 font-semibold rounded-2xl bg-gradient-to-b from-white via-gray-300 to-gray-500 mb-5">Get Plus</button>
+                        <button className="text-black text-3xl justify-center px-6 py-3 w-[70%] font-semibold rounded-[1.5rem] bg-gradient-to-b from-white via-gray-300 to-gray-500 mt-7">
+                            Get Plus
+                        </button>
                     </div>
                 </div>
                 <div className=" bg-white  text-gray-500 text-lg leading-10 rounded-t-3xl rounded-b-3xl px-5 py-5 tracking-tight">
