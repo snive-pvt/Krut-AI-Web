3365849fa0651c03f3c4c4634696c33e49a10976
Prince <princekumar2000.pks@gmail.com>
2024-04-30 15:54:02 +0530

    Added faq

--- src/Components/tools/ToolSlider.jsx
+++ src/Components/tools/ToolSlider.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "./SlideToUnlock.css"; // Import your CSS file for styling
 function ToolSlider() {
   const [isClicked, setIsClicked] = useState(false);
 
--- src/Pages/tools/Tools.jsx
+++ src/Pages/tools/Tools.jsx
@@ -21,9 +21,29 @@ const Tools = () => {
           `,
       },
       {
-        question: "Lorem ipsum dolor sit amet.",
+        question: "What is the easiest way to use AI image generators?",
         answer:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet semper lacus. Integer sit amet ipsum ac turpis.",
+          "Open Krut.ai's Product Studio, just input the  prompt or take a recommendation and get your image in seconds.",
+      },
+      {
+        question: "What are the disadvantages of upscaling?",
+        answer:
+          "OpWhile there are cons of image upscaling like Loss of quality, limited effectiveness, and a restriction of file size, Krut.Ai's Image Upscaler helps you overcome them so there are near to zero disadvantages of upscaling.",
+      },
+      {
+        question: "What is the better alternative for generative fill?         ",
+        answer:
+          "Krut AI is a free AI image generator tool, which offers a huge collection of AI tools to generate and alter your pictures. Its Magic Replace feature works like Photoshop's Generative Fill, but it'seven easier to use..",
+      },
+      {
+        question: "How to remove multiple objects from a picture? ",
+        answer:
+          "Krut AI is a free AI image generator tool, which offers a huge collection of AI tools to generate and alter your pictures. Its Magic Replace feature works like Photoshop's Generative Fill, but it'seven easier to use..",
+      },
+      {
+        question: "Can I use AI-generated images for commercial use? ",
+        answer:
+          "Our terms do not limit your usage but if you want to check for other AI image generators, go to their legal section to know more.",
       },
     ],
   };
